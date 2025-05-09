b826482de8e7a23fa76ddb167d60aaa55027baf4
ESSY555 <adewumiisrael66@gmail.com>
2025-05-09 14:35:34 +0100

    logo added

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -281,6 +281,7 @@ const Index = () => {
       <div className="min-h-screen bg-gradient-to-b from-white to-gray-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
           <div className="text-center mb-12">
+            <img src="../../public/signup-logo.PNG" alt="Description" />
             <h1 className="text-4xl font-bold text-gray-900 mb-4">
               Welcome to QuizJungle
             </h1>
